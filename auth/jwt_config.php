b9c712517ce5c09441c125a40ce8ad682e899708
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-06-26 00:31:56 +0300

    Update jwt_config.php

--- auth/jwt_config.php
+++ auth/jwt_config.php
@@ -1,17 +1,20 @@
 <?php
+
 namespace OAuth\JWT;
 /*
 use function OAuth\JWT\create_jwt;
 use function OAuth\JWT\verify_jwt;
 */
+
 include_once __DIR__ . '/../vendor_load.php';
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
-function create_jwt(string $username): string {
+function create_jwt(string $username): string
+{
     global $jwt_key, $domain;
-  
+
     $payload = [
         'iss' => $domain,     // المصدر
         'iat' => time(),               // وقت الإنشاء
@@ -22,7 +25,8 @@ function create_jwt(string $username): string {
     return JWT::encode($payload, $jwt_key, 'HS256');
 }
 
-function verify_jwt(string $token): object {
+function verify_jwt(string $token): object
+{
     global $jwt_key;
     return JWT::decode($token, new Key($jwt_key, 'HS256'));
 }
