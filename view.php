6d83bbcba3f2a77b9bd0a3905da56eda3419e12e
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-09-06 01:46:54 +0300

    z

--- view.php
+++ view.php
@@ -1,5 +1,6 @@
 <?php
-if (strpos(__FILE__, "I:\\") !== false) {
+
+if (substr(__DIR__, 0, 2) == 'I:') {
 	include_once __DIR__ . '/../mdwiki/public_html/header.php';
 } else {
 	include_once __DIR__ . '/../header.php';
