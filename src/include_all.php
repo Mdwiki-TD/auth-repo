427c856e363bd6c0bd2f612aa28cd4ea76cefd55
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-02-25 06:43:48 +0300

    Remove oauth/send_edit helper
    Delete src/oauth/send_edit.php and remove its include from src/include_all.php. This eliminates the auth_make_edit and get_edits_tokens OAuth helper functions and cleans up the include chain for OAuth utilities.

--- src/include_all.php
+++ src/include_all.php
@@ -11,5 +11,3 @@ include_once __DIR__ . '/oauth/access_helps_new.php';
 include_once __DIR__ . '/oauth/jwt_config.php';
 
 include_once __DIR__ . '/oauth/helps.php';
-
-include_once __DIR__ . '/oauth/send_edit.php';
