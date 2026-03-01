63970a5e59dec544530ca21920ed36661739bf81
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-01 05:26:01 +0300

    Update vendor_load.php

--- src/vendor_load.php
+++ src/vendor_load.php
@@ -1,5 +1,7 @@
 <?php
 
+// NOTE: This file is used in publish-repo
+
 $vendor_path = __DIR__ . '/vendor/autoload.php';
 if (!file_exists($vendor_path)) {
     $vendor_path = __DIR__ . '/../vendor/autoload.php';
