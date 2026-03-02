932f7665c1a9b534417df22d8ca3361171d3ee91
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-02 07:26:27 +0300

    .

--- tests/App/Security/JwtServiceTest.php
+++ tests/App/Security/JwtServiceTest.php
@@ -24,7 +24,7 @@ class JwtServiceTest extends TestCase
     public function testCreateTokenReturnsNonEmptyString(): void
     {
         $token = $this->service->createToken('test_user');
-        
+
         $this->assertNotEmpty($token);
         $this->assertIsString($token);
     }
@@ -33,9 +33,9 @@ class JwtServiceTest extends TestCase
     {
         $username = 'test_user';
         $token = $this->service->createToken($username);
-        
+
         [$verifiedUsername, $error] = $this->service->verifyToken($token);
-        
+
         $this->assertEquals($username, $verifiedUsername);
         $this->assertEmpty($error);
     }
@@ -43,7 +43,7 @@ class JwtServiceTest extends TestCase
     public function testVerifyTokenReturnsErrorForEmptyToken(): void
     {
         [$username, $error] = $this->service->verifyToken('');
-        
+
         $this->assertEmpty($username);
         $this->assertNotEmpty($error);
         $this->assertStringContainsString('required', strtolower($error));
@@ -52,7 +52,7 @@ class JwtServiceTest extends TestCase
     public function testVerifyTokenReturnsErrorForInvalidToken(): void
     {
         [$username, $error] = $this->service->verifyToken('invalid_token_string');
-        
+
         $this->assertEmpty($username);
         $this->assertNotEmpty($error);
     }
@@ -61,9 +61,9 @@ class JwtServiceTest extends TestCase
     {
         $token = $this->service->createToken('test_user');
         $tamperedToken = substr($token, 0, -5) . 'XXXXX';
-        
+
         [$username, $error] = $this->service->verifyToken($tamperedToken);
-        
+
         $this->assertEmpty($username);
         $this->assertNotEmpty($error);
     }
@@ -72,7 +72,7 @@ class JwtServiceTest extends TestCase
     {
         $token = $this->service->createToken('test_user');
         $parts = explode('.', $token);
-        
+
         $this->assertCount(3, $parts, 'JWT should have 3 parts: header, payload, signature');
     }
 
@@ -80,14 +80,14 @@ class JwtServiceTest extends TestCase
     {
         $token1 = $this->service->createToken('user1');
         $token2 = $this->service->createToken('user2');
-        
+
         $this->assertNotEquals($token1, $token2);
     }
 
     public function testIsTokenValidReturnsTrueForValidToken(): void
     {
         $token = $this->service->createToken('test_user');
-        
+
         $this->assertTrue($this->service->isTokenValid($token));
     }
 
@@ -100,11 +100,11 @@ class JwtServiceTest extends TestCase
     public function testMultipleRoundTrips(): void
     {
         $usernames = ['user1', 'user2', 'test_user', 'admin', 'user@example.com'];
-        
+
         foreach ($usernames as $username) {
             $token = $this->service->createToken($username);
             [$verifiedUsername, $error] = $this->service->verifyToken($token);
-            
+
             $this->assertEquals($username, $verifiedUsername, "Failed for: $username");
             $this->assertEmpty($error);
         }
@@ -114,7 +114,7 @@ class JwtServiceTest extends TestCase
     {
         $serviceWithNoKey = new JwtService('', 'localhost');
         $token = $serviceWithNoKey->createToken('test_user');
-        
+
         $this->assertEmpty($token);
     }
 
@@ -122,7 +122,7 @@ class JwtServiceTest extends TestCase
     {
         $serviceWithNoKey = new JwtService('', 'localhost');
         [$username, $error] = $serviceWithNoKey->verifyToken('any_token');
-        
+
         $this->assertEmpty($username);
         $this->assertNotEmpty($error);
     }
@@ -130,11 +130,11 @@ class JwtServiceTest extends TestCase
     public function testFromSettingsFactory(): void
     {
         $service = JwtService::fromSettings();
-        
+
         $username = 'factory_test_user';
         $token = $service->createToken($username);
         [$verifiedUsername, $error] = $service->verifyToken($token);
-        
+
         $this->assertEquals($username, $verifiedUsername);
         $this->assertEmpty($error);
     }
@@ -143,9 +143,9 @@ class JwtServiceTest extends TestCase
     {
         $service1 = new JwtService('secret_key_1', 'localhost');
         $service2 = new JwtService('secret_key_2', 'localhost');
-        
+
         $token = $service1->createToken('test_user');
-        
+
         // Should not verify with different key
         [$username, $error] = $service2->verifyToken($token);
         $this->assertEmpty($username);
@@ -156,15 +156,15 @@ class JwtServiceTest extends TestCase
     {
         // Create service with 1 second expiry
         $shortLivedService = new JwtService($this->testSecretKey, 'localhost', 1);
-        
+
         $token = $shortLivedService->createToken('test_user');
-        
+
         // Token should be valid immediately
         $this->assertTrue($shortLivedService->isTokenValid($token));
-        
+
         // Wait just over 1 second for the token to expire
         usleep(1100000); // 1.1 seconds
-        
+
         [$username, $error] = $shortLivedService->verifyToken($token);
         $this->assertEmpty($username);
         $this->assertStringContainsString('expired', strtolower($error));
