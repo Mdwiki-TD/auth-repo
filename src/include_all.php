530fa54b737a69a9b4ae6e8028f4b11553717f69
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-02 07:09:35 +0300

    Update include_all.php

--- src/include_all.php
+++ src/include_all.php
@@ -13,3 +13,15 @@ include_once __DIR__ . '/oauth/jwt_config.php';
 include_once __DIR__ . '/oauth/helps.php';
 
 include_once __DIR__ . '/oauth/utils.php';
+
+include_once __DIR__ . '/Actions/BaseAction.php';
+include_once __DIR__ . '/Actions/CallbackAction.php';
+include_once __DIR__ . '/Actions/GetUserAction.php';
+include_once __DIR__ . '/Actions/LoginAction.php';
+include_once __DIR__ . '/Actions/LogoutAction.php';
+
+include_once __DIR__ . '/Repository/TokenRepository.php';
+
+include_once __DIR__ . '/Services/CookieService.php';
+include_once __DIR__ . '/Services/EncryptionService.php';
+include_once __DIR__ . '/Services/JwtService.php';
