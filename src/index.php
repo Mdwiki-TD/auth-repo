0f62c5d6c80e7bce9dd4c0388678e3eed08a268c
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-24 04:57:31 +0300

    .

--- src/index.php
+++ src/index.php
@@ -5,12 +5,6 @@ if (isset($_REQUEST['test'])) {
     error_reporting(E_ALL);
 };
 
-$env = getenv('APP_ENV') ?: ($_ENV['APP_ENV'] ?? 'development');
-
-if ($env === 'development' && file_exists(__DIR__ . '/dev/load_env.php')) {
-    include_once __DIR__ . '/dev/load_env.php';
-}
-
 include_once __DIR__ . '/include_all.php';
 
 // length of $_GET == 1 and isset($_GET['test'])
