fd0d6365c488d28e81a1ca38823c1190b889e8f1
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-05-08 05:49:16 +0300

    remove `DB_NAME_NEW` merge databases

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 // Set test environment
 putenv('APP_ENV=testing');
 putenv('DB_HOST_TOOLS=localhost:3306');
-putenv('DB_NAME=s54732__mdwiki');
-putenv('DB_NAME_NEW=s54732__mdwiki_new');
+putenv('DB_NAME=s54732__mdwikiz');
 putenv('TOOL_TOOLSDB_USER=root');
 putenv('TOOL_TOOLSDB_PASSWORD=root11');
 putenv('CONSUMER_KEY=test_consumer_key');
